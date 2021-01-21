92ac70e8d56b211ea7676ac051a45d61e215e7ad
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2021-01-21 15:32:05 +0100

    FIX:	- removed wrong param

--- l10n_it_abicab/__manifest__.py
+++ l10n_it_abicab/__manifest__.py
@@ -1,19 +1,7 @@
 # Copyright 2015 Associazione Odoo Italia (<http://www.odoo-italia.org>)
 # Copyright 2016 Davide Corio (Abstract)
 # Copyright 2018 Sergio Zanchetta (Associazione PNLUG - Gruppo Odoo)
-<<<<<<< HEAD
 # Copyright (C) 2021 Daniel Smerghetto (Omniasolutions snc) www.omniasolutions.eu
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-{
-    'name': 'Italian localization - Codici bancari ABI/CAB',
-    'version': '14.0.0.0.1',
-    'category': 'Localization/Italy',
-    'development_status': 'Production/Stable',
-    'summary': 'Base Bank ABI/CAB codes',
-    'author': "Odoo Italia Network, Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/l10n-italy/tree/12.0/l10n_it_abicab',
-=======
 # Copyright 2020 Matteo Boscolo (https://www.omniasolutions.website)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -25,7 +13,6 @@
     'summary': 'Base Bank ABI/CAB codes',
     'author': "Odoo Italia Network, Odoo Community Association (OCA)",
     'website': 'https://github.com/OCA/l10n-italy/tree/14.0/l10n_it_abicab',
->>>>>>> branch '14.0_develop' of https://github.com/OmniaGit/l10n-italy.git
     'license': 'AGPL-3',
     'depends': ['account'],
     'data': ['views/abicab_view.xml'],
